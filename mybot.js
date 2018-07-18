befca144aa8a9e8a919dfe1dcc6c5624911bc27c
momofoshosho <morgenmshay@gmail.com>
2018-07-18 14:13:16 -0700

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -31,7 +31,7 @@ client.on("message", (message) => {
       ]
       var rand = Math.floor(Math.random() * boopimgs.length);
       var randomboop = boopimgs[rand];
-      const embed = new Discord.RichEmbed().addField("Boop!").attachImage(randomboop)
+      const embed = new Discord.RichEmbed().addField("Boop!").addURL(randomboop)
 message.channel.send({ embed });
   } else
     
