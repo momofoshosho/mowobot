9d696d10f6f2eab513e382c45509836b7e5472f6
momofoshosho <morgenmshay@gmail.com>
2018-07-18 01:37:57 -0700

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -14,50 +14,52 @@ client.on("message", (message) => {
     
   if (message.content.startsWith(prefix + "boop")) {
     message.channel.send("boop boop!");
-      
-  } if (message.content.startsWith("lesbians")) {
+  } else
+    
+  if (message.content.startsWith("lesbians")) {
      message.channel.send("owo are we talking about lesbians? Mowogen and Ken *love* talking about lesbians!");
    } else 
     
-    if (message.content.startsWith("ant is a hoe")) {
-      message.channel.send("you're damn right! >.<");
-    } else
+  if (message.content.startsWith("ant is a hoe")) {
+    message.channel.send("you're damn right! >.<");
+  } else
 
   if (message.content.startsWith(prefix + "owo")) {
     message.channel.send("OwO What's This?");
   } else
-      if (message.content.startsWith(prefix + "help")) {
-          message.channel.send({embed: {
-              author: {
-                  name: client.user.username,
-                  icon_url: client.user.avatarURL
+    
+    if (message.content.startsWith(prefix + "help")) {
+        message.channel.send({embed: {
+            author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL
+            },
+            title: "Look at what I can do!",
+            description: "my default prefix is !",
+            fields: [{
+                name: "prefix",
+                value: "Get my attention with something else :3"
               },
-              title: "Look at what I can do!",
-              description: "my default prefix is !",
-              fields: [{
-                  name: "prefix",
-                  value: "Get my attention with something else :3"
+                {
+                name: "ping",
+                value: "Check out my response time >.<"
+              },
+                {
+                    name: "help",
+                    value: "what? you already know what this does"
                 },
-                  {
-                  name: "ping",
-                  value: "Check out my response time >.<"
-                },
-                  {
-                      name: "help",
-                      value: "what? you already know what this does"
-                  },
-                  {
-                      name: "owo",
-                      value: "*sees buldge*"
-                  }
-              ],
-              timestamp: new Date(),
-              footer: {
-                  icon_url: client.user.avatarURL,
-                  text: "© Mowobot"
-              }
-          }
-       });
-      }
+                {
+                    name: "owo",
+                    value: "*sees buldge*"
+                }
+            ],
+            timestamp: new Date(),
+            footer: {
+                icon_url: client.user.avatarURL,
+                text: "© Mowobot"
+            }
+        }
+     });
+    }
 });
 client.login(process.env.BOT_TOKEN);
