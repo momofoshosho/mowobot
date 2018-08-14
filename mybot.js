af259df396a9ae3bef2e28b352583b633bbfad60
momofoshosho <morgenmshay@gmail.com>
2018-08-13 18:21:05 -0700

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -34,7 +34,7 @@ client.on("message", (message) => {
       //message.channel.send({attachment});
   //} else
     
-  if (message.content.startsWith("lesbians")) {
+  if (message.content.contains("lesbian")) {
      message.channel.send("owo are we talking about lesbians? Mowogen and Ken *love* talking about lesbians!");
    } else 
     
