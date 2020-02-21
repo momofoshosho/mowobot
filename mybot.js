5b63d4bc644cc936b7d68fe9ff1aec9d1f626a94
momofoshosho <morgenmshay@gmail.com>
2020-02-20 21:41:34 -0800

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -51,7 +51,7 @@ client.on("message", (message) => {
   
         if(ratus.user.id === message.author.id) {
          return message.channel.send("**" + message.author.toString()** + " , I'd give you " + {result} + "/10 <:rindo:>");
-        } else return message.channel.send("I'd give **_" + {ratus} + "_** " +  {result} + "/10 <:rindo:>");
+        } else return message.channel.send("I'd give **_" + ${ratus} + "_** " +  ${result} + "/10 <:rindo:>");
       }
       else
     if (message.content.startsWith(prefix + "help")) {
